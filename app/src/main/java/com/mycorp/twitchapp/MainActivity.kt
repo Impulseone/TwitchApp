b68e37fc3e5bb466380691fdaa019f09a02cfa12
Крайнюков Сергей <skrajnjukov@rctahograf.ru>
2021-09-21 18:09:35 +0300

    get response from server

--- app/src/main/java/com/mycorp/twitchapp/MainActivity.kt
+++ app/src/main/java/com/mycorp/twitchapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.mycorp.twitchapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mycorp.twitchapp.databinding.ActivityMainBinding
+import com.mycorp.twitchapp.retrofit.Common
+import com.mycorp.twitchapp.retrofit.TwitchResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        Common.retrofitService.loadGames().enqueue(object: Callback<TwitchResponse> {
+            override fun onFailure(call: Call<TwitchResponse>, t: Throwable) {
+                print("error")
+            }
+
+            override fun onResponse(
+                call: Call<TwitchResponse>,
+                response: Response<TwitchResponse>
+            ) {
+                var twitchResponse = response.body()
+                print("all is fine")
+            }
+        })
+//        activityMainBinding.button.setOnClickListener{
+//
+//        }
     }
 }
---
+++ app/src/main/java/com/mycorp/twitchapp/retrofit/ApiService.kt
@@ -0,0 +1,11 @@
+package com.mycorp.twitchapp.retrofit
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
+interface ApiService {
+    @Headers("Accept: application/vnd.twitchtv.v5+json", "Client-ID: ahuoi1tl0qmqbyi8jo8nitbmuaad7w")
+    @GET("kraken/games/top")
+    fun loadGames(): Call<TwitchResponse>
+}
